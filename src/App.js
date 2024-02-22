7d4325782cb9f379e037a782a381ce86ce2c5fac
Jaden Mu <111476441+777eft@users.noreply.github.com>
2024-02-22 15:37:44 +1100

    Added required files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
-
   const [books, setBooks] = useState();
 
   const getBooks = async () => {
-
     try {
-      const response = await api.get("/api/v1/books/all-books");
-
-      console.log(response.data);
+      const response = await api.get("/books");
       setBooks(response.data);
-
     } catch (error) {
       console.log(error);
-
     }
   }
 
@@ -26,6 +23,12 @@ function App() {
 
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} ></Route>
+
+        </Route>
+      </Routes>
 
     </div>
   );
